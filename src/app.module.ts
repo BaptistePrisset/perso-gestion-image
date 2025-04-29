50c889dba1a0f264109bade1bb7f102076fbab4c
Baptiste Prisset <baptiste.prisset@etudiant.univ-lr.fr>
2025-04-29 14:29:48 +0200

    Add image upload functionality with MySQL integration and validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
 import { Images } from './modules/images/images.entity';
 import { ImagesModule } from './modules/images/images.module';
 
@@ -24,5 +23,5 @@ import { ImagesModule } from './modules/images/images.module';
   providers: [AppService],
 })
 export class AppModule {
-  constructor(private dataSource: DataSource) {}
+  constructor() {}
 }
